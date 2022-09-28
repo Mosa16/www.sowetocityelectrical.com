609abc942a5f4d1dc8b9a0be97acb7dc81ddb888
ProMosa16 <profmosa@gmail.com>
2022-09-28 14:02:04 +0200

    Add project files.

---
+++ www.sowetocityelectrical.com/Pages/inner_page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace www.sowetocityelectrical.com.Pages
+{
+    public class inner_pageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ www.sowetocityelectrical.com/Pages/portfolio_details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace www.sowetocityelectrical.com.Pages
+{
+    public class portfolio_detailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
